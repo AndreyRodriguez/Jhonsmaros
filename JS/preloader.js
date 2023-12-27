7971cd04e5cfe4fe6346f0fd37ae4d9273eb6fad
yilver7u7 <yilversgg@gmail.com>
2023-12-27 15:55:14 -0500

    Se elimino el Footer y se dejo la pagina y sus diferentes secciones como una One-page sin scroll

--- JS/preloader.js
+++ JS/preloader.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         document.getElementById('preloader').style.display = 'none';
         document.getElementById('main').style.display = 'block';
-    }, 2000); // 2000 milisegundos = 2 segundos
+    }, 1000); // 2000 milisegundos = 2 segundos
 });
