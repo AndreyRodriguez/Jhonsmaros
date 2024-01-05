05633f50c7b883909723c43a1bf7f8c7144165dd
AndreyRodriguez <rodriguezladinoandrey@gmail.com>
2024-01-05 00:04:40 -0500

    Se ajustaron colores, tamaños y espacios en las secciones y ahora comencemos del about redirige al contacto

--- JS/card.js
+++ JS/card.js
@@ -1,3 +1,14 @@
 document.getElementById('comencemos').addEventListener('click', function() {
-        window.open('https://wa.me/573116384803?text=Hola%20👋', '_blank');
+        window.open('https://wa.me/573116384803?text=Hola%20', '_blank');
     });
+
+const contenedor = document.querySelector('.container');
+
+contenedor.addEventListener('click', (event) => {
+
+    const elemento = event.target;
+    if (elemento.classList.contains('button-card')) {
+    location.href = './contact.html';
+    }
+});
+    
